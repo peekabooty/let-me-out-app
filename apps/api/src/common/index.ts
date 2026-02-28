60a516c2a69f42bd7089a8e7f37bde86e6d24daa
Daniel Veiga <danielveigadiaz@gmail.com>
2026-02-28 17:33:50 +0000

    feat(auth): Adds global JWT and roles guards

--- apps/api/src/common/index.ts
+++ apps/api/src/common/index.ts
@@ -3,3 +3,6 @@ export * from './clock';
 export * from './request-id';
 export * from './errors';
 export * from './auth/cookies';
+export * from './auth/roles.decorator';
+export * from './auth/roles.guard';
+export * from './auth/public.decorator';
