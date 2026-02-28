1bcb63af37051bd7a446485d746d10fc1a045dbc
Daniel Veiga <danielveigadiaz@gmail.com>
2026-02-28 17:27:37 +0000

    chore(auth): Restore explicit return type

--- apps/api/src/modules/auth/strategies/local.strategy.ts
+++ apps/api/src/modules/auth/strategies/local.strategy.ts
@@ -2,7 +2,7 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy } from 'passport-local';
 
-import { AuthService } from '../auth.service';
+import { AuthService, AuthUser } from '../auth.service';
 
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
@@ -10,7 +10,7 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
     super({ usernameField: 'email' });
   }
 
-  async validate(email: string, password: string) {
+  async validate(email: string, password: string): Promise<AuthUser> {
     try {
       return await this.authService.validateUser(email, password);
     } catch {
